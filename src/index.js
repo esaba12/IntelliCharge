e79b1e89e274feefd67b0096a139587fe09e8de8
Ethan Saba <ethansaba12@gmail.com>
2023-04-11 17:51:57 -0700

    more errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Navbar from './Navbar';
-import Main from './main';
+import Main from './Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About';
 import Buy from './Buy';
